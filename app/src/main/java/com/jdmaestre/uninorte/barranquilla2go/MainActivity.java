38618c5621eb8119c54851daf50997db37d0dfcc
Jose Maestre <jdmaestre@uninorte.edu.co>
2014-11-07 01:36:57 -0500

    ActionBar Backgroun:Black

--- app/src/main/java/com/jdmaestre/uninorte/barranquilla2go/MainActivity.java
+++ app/src/main/java/com/jdmaestre/uninorte/barranquilla2go/MainActivity.java
@@ -32,6 +32,7 @@ public class MainActivity extends Activity implements ActionBar.TabListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         ParseUser currentUser = ParseUser.getCurrentUser();
         if (currentUser == null){
             navigateToLogin();
